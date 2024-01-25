1b8e23548462c98d2f5b6595bdbc8d5d512dd2d0
vasiliy-klimkin <vasily.klimkin@gmail.com>
2024-01-25 14:47:43 -0500

    refactor

--- index.js
+++ index.js
@@ -3,31 +3,14 @@
 // which will be numbers, and adds them toghether 
 // in and returns a sum of all the numbers.
 
-
-// Unknowns
-
-// What is a command line arguments?
-// console.log(process.argv); // the arguments start from 2nd index of the array
-// How do i get them?
-// return a number ?
-
-// Knowns
-// adding a number  (+)
-// adding multiple numbers ( probably an array ) loop through numbers and use the + command
-
-// cntr + / or cmd + /
+// const array = process.argv;
 
 
-// looping through command line arguments
-const sum = function() {
-  const array = process.argv;
-  //          [1,2,3,4,5,6,7,8]
-  // index     0 1 2 3 4 
+const sum = function(array) {
   
   console.log("loop");
   let result = 0;
-  for (let i = 2; i < array.length; i++) {
-    // console.log(i);
+  for (let i = 0; i < array.length; i++) {
     // +array[i] shorthand
     result = result + Number(array[i]);
     console.log(array[i]);
@@ -36,5 +19,13 @@ const sum = function() {
   return result;
 }
 
-const value = sum();
+//  Test Cases -----------------------------------------
+const arr2 = [9,9,1,1];
+const value = sum(arr2);
 console.log('the sum is ', value);
+
+
+const commandLineArgumentsArray = process.argv.slice(2);
+const value2 = sum(commandLineArgumentsArray);
+console.log('the sum is ', value2);
+
