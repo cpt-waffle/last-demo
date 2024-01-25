a632a889f03b52a569a5613988b5662bd010df99
vasiliy-klimkin <vasily.klimkin@gmail.com>
2024-01-25 14:28:28 -0500

    add readme changes and figured out command line arguments

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// We are tasked to create a program that takes 
+// in any number of command line arguments
+// which will be numbers, and adds them toghether 
+// in and returns a sum of all the numbers.
+
+
+// Unknowns
+
+// What is a command line arguments?
+console.log(process.argv); // the arguments start from 2nd index of the array
+// How do i get them?
+// return a number ?
+
+// Knowns
+// adding a number  (+)
+// adding multiple numbers ( probably an array ) loop through numbers and use the + command
+
+// cntr + / or cmd + /
